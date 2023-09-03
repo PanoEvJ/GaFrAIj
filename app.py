771042105f670e317418dcfe34f0481296c10d9f
PanoEvJ <panosevag@gmail.com>
2023-09-03 10:45:38 +0200

    minor fix

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-# main
+import genie_kernel as genie
+
+
+
+Bm13.section = UB_178x102x19;
+
+Bm14.section = UB_178x102x19;
---
+++ cross_section_library.py
@@ -0,0 +1,15 @@
+def section_lib():
+    # This library shall be used from GaFra to select appropriate section sizes for the code checks – Parameter 1
+    UB_127x76x13 = ISection(0.127 m, 0.076 m, 0.004 m, 0.0076 m, 0.0076 m);
+    UB_127x76x13.description = "BS4 Part1 1993 lib: UB 127x76x13";
+    UB_127x76x13.libraryGeneralSection = GeneralSection(0.00165 m^2, 2.85e-08 m^4, 4.73e-06 m^4, 5.57e-07 m^4, 0 m^4, 4.212430596e-06 m^3, 7.46e-05 m^3, 1.47e-05 m^3, 0.00075648734 m^2, 0.0004500664202 m^2, 0 m, 0 m, 4.207594209e-05 m^3, 1.1198e-05 m^3, 8.42e-05 m^3, 2.26e-05 m^3);
+    UB_152x89x16 = ISection(0.1524 m, 0.0887 m, 0.0045 m, 0.0077 m, 0.0076 m);
+    UB_152x89x16.description = "BS4 Part1 1993 lib: UB 152x89x16";
+    UB_152x89x16.libraryGeneralSection = GeneralSection(0.00203 m^2, 3.56e-08 m^4, 8.34e-06 m^4, 8.98e-07 m^4, 0 m^4, 5.260389739e-06 m^3, 0.000109 m^3, 2.02e-05 m^3, 0.0008920729847 m^2, 0.0006091595876 m^2, 0 m, 0 m, 6.162785522e-05 m^3, 1.54920845e-05 m^3, 0
+    UC_203x203x60.description = "BS4 Part1 1993 lib: UC 203x203x60";
+    UC_203x203x60.libraryGeneralSection = GeneralSection(0.00764 m^2, 4.72e-07 m^4, 6.125e-05 m^4, 2.065e-05 m^4, 0 m^4, 4.055728897e-05 m^3, 0.000584 m^3, 0.000201 m^3, 0.003848446 m^2, 0.001754982899 m^2, 0 m, 0 m, 0.0003280375219 m^3, 0.000152356776 m^3, 0.000656 m^3, 0.000305 m^3);
+    UB_203x133x25 = ISection(0.2032 m, 0.1332 m, 0.0057 m, 0.0078 m, 0.0076 m);
+    UB_203x133x25.description = "BS4 Part1 1993 lib: UB 203x133x25";
+    UB_203x133x25.libraryGeneralSection = GeneralSection(0.0032 m^2, 5.96e-08 m^4, 2.34e-05 m^4, 3.08e-06 m^4, 0 m^4, 8.9534922e-06 m^3, 0.00023 m^3, 4.62e-05 m^3, 0.001357161846 m^2, 0.001035104249 m^2, 0 m, 0 m, 0.0001288651196 m^3, 3.53592585e-05 m^3, 0.000258 m^3, 7.09e-05 m^3);
+
+
